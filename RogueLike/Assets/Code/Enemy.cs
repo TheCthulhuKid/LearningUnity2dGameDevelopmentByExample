5890a9314212d3297acd8ea2956429a9c4688afe
Marc-Anthony Taylor <gileadslostson@gmail.com>
2014-09-28 22:16:53 +0200

    Implemented enemy animation, combat and permadeath.

--- RogueLike/Assets/Code/Enemy.cs
+++ RogueLike/Assets/Code/Enemy.cs
@@ -3,13 +3,102 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+	GameObject heroObj;
+	float enemySpeed;
+
+	//	These values are present in the books example code but I see no use for them
+//	bool enemyRight = false;
+//	bool enemyUp = false;
+//	bool enemyLeft = false;
+//	bool enemyDown = false;
+	
+	Animator enemyAnimator;
+
 	// Use this for initialization
 	void Start () {
-	
+		enemySpeed = 1.0f;
+
+		//	The book states that the "InvokeRepeating" calls should be made outwith a function. In C# this isn't possible
+		InvokeRepeating("Accelerate", 2, 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		EnemyMove ();
+	}
+
+	void EnemyMove(){
+		heroObj = GameObject.Find ("hero");
+		enemyAnimator = (Animator)GetComponent ("Animator");
+
+		if (heroObj != null)
+		{
+			if (transform.position.y > heroObj.transform.position.y)
+			{
+				
+				enemyAnimator.SetBool("enemyLeft", false);
+				enemyAnimator.SetBool("enemyUp", false);
+				enemyAnimator.SetBool("enemyDown", true);
+				enemyAnimator.SetBool("enemyRight", false);
+//				enemyDown = true;
+//				enemyLeft = false;
+//				enemyRight = false;
+//				enemyUp = false;
+				transform.Translate(Vector3.down * enemySpeed * Time.deltaTime);
+				
+			}
+			else
+			{
+				enemyAnimator.SetBool("enemyLeft", false);
+				enemyAnimator.SetBool("enemyUp", true );
+				enemyAnimator.SetBool("enemyDown", false );
+				enemyAnimator.SetBool("enemyRight", false );
+//				enemyDown = false;
+//				enemyLeft = false;
+//				enemyRight = false;
+//				enemyUp = true;
+				transform.Translate(Vector3.up * enemySpeed * Time.deltaTime);
+			}
+			
+			if (transform.position.x < heroObj.transform.position.x)
+			{
+				
+				enemyAnimator.SetBool("enemyLeft", false);
+				enemyAnimator.SetBool("enemyUp", false );
+				enemyAnimator.SetBool("enemyDown", false );
+				enemyAnimator.SetBool("enemyRight", true );
+//				enemyDown = false;
+//				enemyLeft = false;
+//				enemyRight = true;
+//				enemyUp = false;
+				transform.Translate(Vector3.right * enemySpeed * Time.deltaTime);
+			}
+			else
+			{
+				enemyAnimator.SetBool("enemyLeft", true);
+				enemyAnimator.SetBool("enemyUp", false );
+				enemyAnimator.SetBool("enemyDown", false );
+				enemyAnimator.SetBool("enemyRight", false );
+//				enemyDown = false;
+//				enemyLeft = true;
+//				enemyRight = false;
+//				enemyUp = false;
+				transform.Translate(Vector3.left * enemySpeed * Time.deltaTime);
+			}
+		}
+	}
 	
+	void Accelerate()
+	{
+		enemySpeed = enemySpeed + 1;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.name=="orb(Clone)")
+		{
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
 	}
 }
--- RogueLike/Assets/Code/Hero.cs
+++ RogueLike/Assets/Code/Hero.cs
@@ -12,20 +12,50 @@ public class Hero : MonoBehaviour {
 
 	Animator animator;
 
+	//	These must be public in order to assign objects via the inspector
 	public Rigidbody2D enemy;
+	public Rigidbody2D orb;
+
+	float orbSpeed = 20f;
+	float orbSpeed2 = -20f;
 
 	// Use this for initialization
 	void Start () {
 		animator = (Animator)GetComponent("Animator");
 
 		EnemySpawn ();
+		//	The book states that the "InvokeRepeating" calls should be made outwith a function. In C# this isn't possible
 		InvokeRepeating("EnemySpawn", 3, 3);
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		MoveCharacter ();
+		//	TODO: I am doing something wrong here the orb fires strangely
+		if(Input.GetButtonDown("Fire1"))
+		{
+			var orbInstance = (Rigidbody2D)Instantiate(orb, transform.position, Quaternion.Euler(new Vector3(-1,0,0)));
+			
+			if (right)
+			{
+				orbInstance.velocity = new Vector2(orbSpeed, 0);
+			}
+
+			if (left)
+			{
+				orbInstance.velocity = new Vector2(orbSpeed2, 0);
+			}
+			
+			if (up)
+			{
+				orbInstance.velocity = new Vector2(0, orbSpeed);
+			}
+
+			if (down)	
+			{
+				orbInstance.velocity = new Vector2(0, orbSpeed2);
+			}
+			
+		}
 	}
 
 	void FixedUpdate () {
@@ -33,6 +63,8 @@ public class Hero : MonoBehaviour {
 	}
 	
 	void MoveCharacter(){
+
+		//	TODO: Use xBox controller
 
 		if (Input.GetKey(KeyCode.A)) {
 			animator.SetBool("left", true);
@@ -83,6 +115,23 @@ public class Hero : MonoBehaviour {
 
 	void EnemySpawn()
 	{
+		//	Why does a variable have to be assigned?
 		var enemyInstance = (Rigidbody2D)Instantiate (enemy, new Vector3(Random.Range (2, 8), Random.Range (-4, 4), 0), Quaternion.Euler (new Vector3 (0, 0, 0)));
+	}
+
+	void OnGUI()
+	{
+		//	TODO: Alter formatting
+		GUI.Box(new Rect(10,10,100,90), "" + Time.time);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//	TODO: Add death effect
+		if((other.gameObject.name=="enemy(Clone)")||(other.gameObject.name=="right")||(other.gameObject.name=="left")||(other.gameObject.name=="bottom")||(other.gameObject.name=="top"))
+		{
+			Time.timeScale = 0;
+			Destroy(gameObject);
+		}
 	}
 }
