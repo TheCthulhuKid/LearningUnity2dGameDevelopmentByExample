7ff4671463690e0635dc9b66a2d6ff8ebea7656d
Marc-Anthony Taylor <gileadslostson@gmail.com>
2014-09-30 23:02:51 +0200

    Played with bg animation

--- EndlessRunner/Assets/Code/Ninja.cs
+++ EndlessRunner/Assets/Code/Ninja.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ninja : MonoBehaviour {
+public class Ninja : MonoBehaviour
+{
+	Animator animator;
+	public bool jump = false;
+	public bool slide = false;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		animator = (Animator)GetComponent("Animator");
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+
+	}
+
+	//	Used instead of Update because we are using a rigid body
+	void FixedUpdate()
+	{
+		MoveCharacter();
+	}
+
+	void MoveCharacter()
+	{
+		//	Here I have simplified the statements to avoid the multiple if/else
+		jump = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+		animator.SetBool("jump", jump);
+		slide = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+		animator.SetBool("slide", slide);
 	}
 }
