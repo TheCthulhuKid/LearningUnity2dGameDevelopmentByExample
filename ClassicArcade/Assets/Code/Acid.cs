2e1a6b391358c164ed0a857faaa2e6da88cac4af
Marc-Anthony Taylor <gileadslostson@gmail.com>
2014-09-30 18:29:06 +0200

    Added arrow controls

---
+++ ClassicArcade/Assets/Code/Acid.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Acid : MonoBehaviour
+{
+	public Rigidbody2D acid;
+
+	float speed = -3.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("SpawnAcid", 3, 7);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void SpawnAcid()
+	{
+		var acidInstance = (Rigidbody2D)Instantiate(acid, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+		
+		acidInstance.name = "Acid(Clone)";
+		acidInstance.velocity = new Vector2(0, speed);
+	}
+}
---
+++ ClassicArcade/Assets/Code/Grime.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grime : MonoBehaviour
+{
+	public Rigidbody2D grime;
+
+	float speed = -3.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("SpawnGrime", 2, 7);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void SpawnGrime()
+	{
+		var grimeInstance = (Rigidbody2D)Instantiate(grime, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+
+		grimeInstance.name = "Grime(Clone)";
+		grimeInstance.velocity = new Vector2(0, speed);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.name.Equals("Spongy"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
